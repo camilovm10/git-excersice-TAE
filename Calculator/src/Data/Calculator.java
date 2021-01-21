2f7581a1226241ab70aee92e195a8a20eb92a81b
Camilo Vasquez <camilovasquez9738@hotmail.com>
2021-01-21 17:30:44 -0500

    multiply and divide methods added

--- Calculator/src/Data/Calculator.java
+++ Calculator/src/Data/Calculator.java
@@ -2,6 +2,12 @@ package Data;
 
 public class Calculator {
 
+    public int multiplyMethod(int num1, int num2) {
+        return num1 * num2;
+    }
 
+    public int divitionMethod(int num1, int num2) {
+        return num1 / num2;
+    }
 
 }
